175021124d40bd98c39e2c365c86fdbe3ce26a4d
Miloš Nikolić <nikolic.milos86@gmail.com>
2024-08-16 22:59:35 +0300

    Update layout.js

--- src/app/(sub pages)/layout.js
+++ src/app/(sub pages)/layout.js
@@ -3,7 +3,7 @@ import HomeBtn from "@/components/HomeBtn";
 
 export default function SubPagesLayout({ children }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center py-20 px-32">
+    <main className="flex min-h-screen flex-col items-center justify-center py-20 px-30">
         <HomeBtn />
         {children}
     </main>
