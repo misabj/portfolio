2e1797b755ae78072250d15daa2188a046258d96
Miloš Nikolić <nikolic.milos86@gmail.com>
2025-10-23 09:41:26 +0200

    Update client and experience numbers in About section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -29,7 +29,7 @@ const AboutDetails = () => {
           className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
         >
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            24+ <sub className="font-semibold text-base">clients</sub>
+            54+ <sub className="font-semibold text-base">clients</sub>
           </p>
         </ItemLayout>
 
@@ -37,7 +37,7 @@ const AboutDetails = () => {
           className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
         >
           <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            5+{" "}
+           7+{" "}
             <sub className="font-semibold text-base">years of experience</sub>
           </p>
         </ItemLayout>
